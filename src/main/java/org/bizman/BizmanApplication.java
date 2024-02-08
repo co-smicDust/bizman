4410472b4f3f6e44f70ea2acc072e7e9d26a28ff
Soyeong Hwangbo <co-smicDust>
2024-02-08 21:49:35 +0900

    프로젝트 시작

---
+++ src/main/java/org/bizman/BizmanApplication.java
@@ -0,0 +1,13 @@
+package org.bizman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizmanApplication.class, args);
+	}
+
+}
