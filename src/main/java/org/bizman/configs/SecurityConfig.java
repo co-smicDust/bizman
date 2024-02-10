6404b15bebfee415f7ed024f0f6164a84ab1473d
co-smicDust <vivichu90@sookmyung.ac.kr>
2024-02-10 16:13:59 +0900

    security 설정

--- src/main/java/org/bizman/configs/SecurityConfig.java
+++ src/main/java/org/bizman/configs/SecurityConfig.java
@@ -1,14 +1,26 @@
 package org.bizman.configs;
 
+import org.bizman.configs.jwt.CustomJwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity   // @PreAuthorize("hasAuthority('ADMIN'))
 public class SecurityConfig {
+
+    @Autowired
+    private CustomJwtFilter customJwtFilter;
+    @Autowired
+    private CorsFilter corsFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // 일단 활성화, 설정 무력화만. 점점 설정 추가해나감.
---
+++ src/main/java/org/bizman/configs/jwt/CustomJwtFilter.java
@@ -0,0 +1,49 @@
+package org.bizman.configs.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+
+// 토큰이 넘어오면 로그인 유지
+@Component
+@RequiredArgsConstructor
+public class CustomJwtFilter extends GenericFilterBean {
+
+    private final TokenProvider tokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest)request;
+
+        /* 요청 헤더 Authorization 항목의 JWT 토큰 추출 S */
+        String header = req.getHeader("Authorization");
+        String jwt = null;
+        if (StringUtils.hasText(header)) {  // == (header != null && !header.isBlank())
+            // Bearer .... 배열 뒤 7번째부터
+            jwt = header.substring(7);
+        }
+        /* 요청 헤더 Authorization 항목의 JWT 토큰 추출 E */
+
+        /* 로그인 유지 처리 S */
+        if (StringUtils.hasText(jwt)) {
+            tokenProvider.validateToken(jwt);   // 토큰에 이상 있을 경우 예외 발생
+
+            Authentication authentication = tokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        /* 로그인 유지 처리 E */
+
+        chain.doFilter(request, response);
+    }
+}
